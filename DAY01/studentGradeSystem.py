3558af1ef99946f7d7da9bb4d6eabd472ee2d633
Muhammad Qaim <227011788+MQaim47@users.noreply.github.com>
2026-07-10 16:12:00 +0500

    Create studentGradeSystem.py

---
+++ DAY01/studentGradeSystem.py
@@ -0,0 +1,31 @@
+name=input("Enter Student Name:")
+std_class=input("Enter Class:")
+
+num_subjects=int(input("Enter Number of Subjects:"))
+total=0
+
+for i in range(num_subjects):
+    subject=input(f"Enter Subject{i+1}:")
+    marks=float(input(f"Enter marks for {subject}:"))
+    total += marks
+    print("\n")
+    
+average=total/num_subjects
+
+if average>=90:
+    grade="A"
+    
+elif average>=80:
+    grade="B"
+elif average>=70:
+    grade="C"
+elif average>=50:
+    grade="D"
+else:
+    grade="F"
+    
+print("\n----- Result -----")
+print("Name:", name)
+print("Class:", std_class)
+print("Average:", average)
+print("Grade:", grade)
